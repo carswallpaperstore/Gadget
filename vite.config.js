018c10312a7ce05a60a0401adb051618388b2cda
carswallpaperstore <carswallpaperstore@gmail.com>
2025-08-17 14:54:04 +0530

    changes in

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ensures compiled JS paths are correct
-});
+  base: '/',   // Vercel साठी हे ठेवा
+})
