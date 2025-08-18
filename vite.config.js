a9dd4cb23c8deda0a9d32ebd8b84655b4a3169a4
carswallpaperstore <carswallpaperstore@gmail.com>
2025-08-18 10:24:55 +0530

    blog 5

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",   // Vercel साठी योग्य
   build: {
-    outDir: "dist",
-  },
-  server: {
-    historyApiFallback: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
   }
 });
